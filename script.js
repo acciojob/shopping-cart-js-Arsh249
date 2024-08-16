a3c73354bbb491383e3c0adf8c5bb8a562ed9f95
Arsh Ilahi <156427743+Arsh249@users.noreply.github.com>
2024-08-16 11:21:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your code here
+window.onload = function() {
+    displayCartItems();
+};
+
+function addToCart(product) {
+    let cart = JSON.parse(localStorage.getItem('cart')) || [];
+    cart.push(product);
+    localStorage.setItem('cart', JSON.stringify(cart));
+    displayCartItems();
+}
+
+function displayCartItems() {
+    const cartItems = document.getElementById('cartItems');
+    cartItems.innerHTML = '';
+
+    const cart = JSON.parse(localStorage.getItem('cart')) || [];
+
+    cart.forEach((item, index) => {
+        const li = document.createElement('li');
+        li.textContent = item;
+        cartItems.appendChild(li);
+    });
+}
+
+function clearCart() {
+    localStorage.removeItem('cart');
+    displayCartItems();
+}
